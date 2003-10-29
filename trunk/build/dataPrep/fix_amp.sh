ff434dbc01de1606d4b35835c2f27922c0bea6cd
rsutton <rsutton@b1811ce5-92dd-4818-80cb-ca98dbf658eb>
2003-10-29 22:49:47 +0000

    added more regular expressions (more cases to fix)

--- trunk/build/dataPrep/fix_amp.sh
+++ trunk/build/dataPrep/fix_amp.sh
@@ -5,7 +5,13 @@
 
 set exscript = "/tmp/fix_amp";
 
+## bare ampersand (space afterwards)
 echo '1,$s/& /\&amp; /g' > $exscript;
+## ampersand at the end of a line
+echo '1,$s/&$/\&amp;/g' > $exscript;
+## ampersand followed by any one of comma, period, tag, or double quote
+echo '1,$s/&\([,\.<"]\)/\&amp;\1/g' > $exscript;
+## FIXME: how to match for &' ?
 echo "write" >> $exscript;
 echo "quit" >> $exscript;
 
